7c51dc2fb316f5ab07b21faf8cf8ea86ad9012cd
PetaMem <petamem@cpan.org>
2013-02-05 18:17:40 +0000

    Import of PETAMEM/Lingua-NOR-Word2Num-0.0577 from CPAN.
    gitpan-cpan-distribution: Lingua-NOR-Word2Num
    gitpan-cpan-version:      0.0577
    gitpan-cpan-path:         PETAMEM/Lingua-NOR-Word2Num-0.0577.tar.gz
    gitpan-cpan-author:       PETAMEM
    gitpan-cpan-maturity:     released

--- lib/Lingua/NOR/Word2Num.pm
+++ lib/Lingua/NOR/Word2Num.pm
@@ -1,7 +1,7 @@
 # For Emacs: -*- mode:cperl; mode:folding; coding:utf-8; -*-
 
 package Lingua::NOR::Word2Num;
-# ABSTRACT: Lingua::NOR::Word2Num is module for converting text containing number representation in Norwegian back into number. Converts whole numbers from 0 up to 999 999 999.
+# ABSTRACT: Word 2 number conversion in NOR.
 
 # {{{ use block
 
@@ -14,10 +14,9 @@ use Parse::RecDescent;
 # }}}
 # {{{ variable declarations
 
-my($ver)      = ('$Rev: 440 $' =~ m{(\d+)}xms);
-our $VERSION = $ver / 10_000;
+our $VERSION = 0.0577;
 our $INFO    = {
-    rev  => '$Rev: 440 $',
+    rev  => '$Rev: 577 $',
 };
 
 my $parser = no_numerals();
@@ -141,12 +140,12 @@ Lingua::NOR::Word2Num
 
 =head1 VERSION
 
-version 0.044
+version 0.0577
 
 text to positive number convertor for Norwegian.
 Input text must be encoded in utf-8.
 
-=head2 $Rev: 440 $
+=head2 $Rev: 577 $
 
 ISO 639-3 namespace.
 
@@ -159,6 +158,8 @@ ISO 639-3 namespace.
  print defined($num) ? $num : "sorry, can't convert this text into number.";
 
 =head1 DESCRIPTION
+
+Word 2 number conversion in NOR.
 
 Lingua::NOR::Word2Num is module for converting text containing number
 representation in Norwegian back into number. Converts whole numbers
